a1db33906afd8ec385285fe3ef7ea7ac50b9779d
KhalidRmb <khalidriyazgit@gmail.com>
2019-03-24 13:06:18 +0530

    L

--- tests/unit/challenges/test_views.py
+++ tests/unit/challenges/test_views.py
@@ -2682,7 +2682,7 @@ class CreateChallengeUsingZipFile(APITestCase):
         expected = {
         'error': self.message
                     }
-        response = self.client.post(self.url, {'zip_configuration': z.read()}, content_type='multipart/form-data')
+        response = self.client.post(self.url, {'zip_configuration': challenge_zip_file}, content_type='multipart/form-data')
         self.assertEqual(response.data, expected)
         self.assertEqual(response.status_code, self.status_code)
 
