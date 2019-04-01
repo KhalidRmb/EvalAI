5167eda22d1ba159bacc06be7beaea7f7bc2eae2
KhalidRmb <khalidriyazgit@gmail.com>
2019-04-01 22:06:43 +0530

    writestr

--- tests/unit/challenges/test_views.py
+++ tests/unit/challenges/test_views.py
@@ -2662,6 +2662,7 @@ class CreateChallengeUsingZipFile(APITestCase):
 
         challengezip.close()
         z = open(join(self.BASE_TEMP_LOCATION,'challenge_zip.zip'), 'rb')
+        contents = z.read()
         '''zip_file = SimpleUploadedFile(
             z.name,
             z.read(),
@@ -2670,7 +2671,7 @@ class CreateChallengeUsingZipFile(APITestCase):
         expected = {
         'error': self.message
                     }
-        response = self.client.post(self.url, {'zip_configuration': z})
+        response = self.client.post(self.url, {'zip_configuration': contents})
         self.assertEqual(response.data, expected)
         self.assertEqual(response.status_code, self.status_code)
 
