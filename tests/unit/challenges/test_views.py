76768be5fb5b2e7c537deddd6b04ebf6e36244b8
KhalidRmb <khalidriyazgit@gmail.com>
2019-04-01 20:36:18 +0530

    rem ignore errors & add binary mode

--- tests/unit/challenges/test_views.py
+++ tests/unit/challenges/test_views.py
@@ -2659,7 +2659,7 @@ class CreateChallengeUsingZipFile(APITestCase):
                         challengezip.write(f)
 
         challengezip.close()
-        z = open(join(self.BASE_TEMP_LOCATION,'challenge_zip.zip'), 'r', errors='ignore')
+        z = open(join(self.BASE_TEMP_LOCATION,'challenge_zip.zip'), 'rb')
         '''zip_file = SimpleUploadedFile(
             z.name,
             z.read(),
